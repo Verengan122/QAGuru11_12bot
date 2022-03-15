af569e5053ff3916fdd626639d93751b20caa6ec
verengan122 <Crysis00!>
2022-03-15 16:41:01 +0300

    bot

---
+++ src/test/java/newTestForm/testBase/TestBase.java
@@ -0,0 +1,32 @@
+package newTestForm.testBase;
+
+import com.codeborne.selenide.Configuration;
+import newTestForm.attach.Attach;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+
+public class TestBase {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadTimeout = 100000;
+//        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+        closeWebDriver();
+    }
+}
