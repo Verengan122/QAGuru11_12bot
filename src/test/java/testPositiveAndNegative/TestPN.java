e1b30cce35d83fa868a637a50b5d405dcada6422
verengan122 <Crysis00!>
2022-03-15 19:54:06 +0300

    bot

---
+++ src/test/java/testPositiveAndNegative/TestPN.java
@@ -0,0 +1,48 @@
+package testPositiveAndNegative;
+
+import io.qameta.allure.Step;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestPN {
+
+    @Test
+    @Step("Тест 1")
+    void test01() {
+        assertTrue(true);
+    }
+
+    @Test
+    @Step("Тест 2")
+    void test02() {
+        assertTrue(true);
+    }
+
+    @Test
+    @Step("Тест 3")
+    void test03() {
+        assertTrue(true);
+    }
+
+    @Test
+    @Step("Тест 4")
+    void test04() {
+        assertTrue(false);
+    }
+
+    @Test
+    @Step("Тест 5")
+    void test05() {
+        assertTrue(false);
+    }
+
+    @Test
+    @Step("Тест 6")
+    void test06() {
+        assertTrue(false);
+    }
+
+
+}
+
